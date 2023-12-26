46d54ffc9c53a5bd5f0469ecf566f0ec1ba78231
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-26 08:32:16 +0530

    Using Alter commands

---
+++ SQL 14th Table.sql
@@ -0,0 +1,22 @@
+create database college;
+use college;
+create table student1(
+rollno int primary key,
+name varchar(50),
+marks int not null,
+grade varchar(1),
+city varchar(20)
+);
+insert into student1 values(101,"Anil",78,"c","pune"),
+(102,"Bhumika",93,"A","pune"),
+(103,"Chetan",78,"B","pune"),
+(104,"dhruv",96,"A","pune"),
+(105,"Emanuael",12,"F","Delhi"),
+(106,"farah",82,"B","Delhi");
+alter table student1
+add column age int;
+alter table student1
+drop column age ;
+alter table student1
+rename to studentexists;
+select * from studentexists;
