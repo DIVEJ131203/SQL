83a18faeb1ecf580bd17c0674a39d230f778798e
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-26 07:23:26 +0530

    Updating table

---
+++ SQL 9th Table.sql
@@ -0,0 +1,19 @@
+create database college;
+use college;
+create table student(
+rollno int primary key,
+name varchar(50),
+marks int not null,
+grade varchar(1),
+city varchar(20)
+);
+insert into student values(101,"Anil",78,"c","pune"),
+(102,"Bhumika",93,"A","pune"),
+(103,"Chetan",78,"B","pune"),
+(104,"dhruv",96,"A","pune"),
+(105,"Emanuael",12,"F","Delhi"),
+(106,"farah",82,"B","Delhi");
+ update student 
+ set grade = "O"
+ where grade="A";
+ select * from student;
