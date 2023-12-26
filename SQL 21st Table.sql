8761015e41e41356540cbdca2d45221d4ee99110
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-26 18:08:26 +0530

    Right Exclusive Join

---
+++ SQL 21st Table.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE divej123;
+USE divej123;
+
+CREATE TABLE student(
+id int primary key,
+name varchar(50)
+);
+INSERT INTO student values
+(101,"divej"),
+(102,"Reeva"),
+(103,"motu"),
+(104,"hello"),
+(105,"don");
+
+CREATE TABLE course(
+course_id int primary key,
+name varchar(30)
+);
+INSERT INTO course values
+(101,"cs"),
+(102,"nothing"),
+(103,"Cse");
+
+select * from student as a
+right join course as b
+on a.id=b.course_id
+where b.course_id=NULL;
