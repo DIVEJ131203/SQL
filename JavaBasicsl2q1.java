0d1a532d86ae7ed10c02615fbddc0b500bacd9c6
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-30 21:51:40 +0530

    Lecture 3

---
+++ JavaBasicsl2q1.java
@@ -0,0 +1,18 @@
+public class JavaBasicsl2q1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int x = (a + b);
+        int y = (a - b);
+        int z = (a * b);
+        int xy = (a / b);
+        int xz = (a % b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(xy);
+        System.out.println(xz);
+    }
+}
